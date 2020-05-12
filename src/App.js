e31c05bbd4117b73edb9725ab75ba405dd55890a
henrique <hbaetaleite@gmail.com>
2020-05-12 19:06:39 -0300

    requisito 3, primeiro commit Baeta

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
 import Categories from './components/Categories';
+import CartPage from './components/CartPage';
 
 class App extends React.Component {
   render() {
     return (
-      <Categories />
+      <main>
+        <Categories />
+        <CartPage />
+      </main>
     );
   }
 }
---
+++ src/components/CartPage.js
@@ -0,0 +1,20 @@
+import React from 'react';
+// import * as api from '../services/api';
+import cart from './cart.png'
+
+class CartPage extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <section>
+        <button name-testid="shopping-cart-button"><img src={cart} width="70px" alt="cart-icon" /></button>
+        <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
+      </section>
+    );
+  }
+}
+
+export default CartPage;
