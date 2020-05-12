6ee37ef71f4e98279f14fbbcf7072d2936ad7d8d
fabemiliano <fab.emiliano@gmail.com>
2020-05-12 19:24:54 -0300

    requisite 5

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import './App.css';
 import Categories from './components/Categories';
+import ProductList from './components/ProductList';
 
 class App extends React.Component {
+
+
   render() {
+    const teste = ['banana', 'laranja'];
     return (
-      <Categories />
+      <div>
+        <Categories />
+        <ProductList products={teste} />
+      </div>
     );
   }
 }
