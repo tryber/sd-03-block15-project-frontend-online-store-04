61cfa6b40681ba941962524ab6372c6d154eccc8
henrique <hbaetaleite@gmail.com>
2020-05-13 15:50:46 -0300

    Criando componente ProductDetails

---
+++ src/components/ProductDetails.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import ButtonCart from './ButtonCart';
+
+export default class ProductDetail extends Component() {
+  render() {
+    return (
+      <section>
+        <div>
+          <h3 data-testid="product-detail-name">Nome do produto - {this.props.price}</h3>
+          <img src={this.props.imgpath} alt={this.props.name}/>
+          <p>Especificação Tecnica</p>
+          <p>{this.props}</p>
+        </div>
+        <ButtonCart />
+      </section>
+    )
+  }
+}
