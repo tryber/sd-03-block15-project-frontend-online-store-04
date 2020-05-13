b130fdb558968586e6e84c90209ba5bbc1a17473
henrique <hbaetaleite@gmail.com>
2020-05-13 18:48:56 -0300

    Estilizacao

--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -1,18 +1,31 @@
 import React, { Component } from 'react';
 import ButtonCart from './ButtonCart';
+import './ProductDetails.css';
 
 export default class ProductDetails extends Component() {
   render() {
     const { name, price, tecs, imgpath } = this.props;
     return (
       <section>
-        <div>
-          <h3 data-testid="product-detail-name">Nome do produto - {price}</h3>
-          <img src={imgpath} alt={name} />
-          <p>Especificação Tecnica</p>
-          <p>{tecs}</p>
+
+        <div className="headerDetails">
+          <ButtonCart />
         </div>
-        <ButtonCart />
+
+        <div className="productContainer">
+
+          <div className="imgContainer">
+            <h3 data-testid="product-detail-name">Nome do produto - {price}</h3>
+            <img src={imgpath} alt={name} />
+          </div>
+
+          <div className="specs">
+            <p>Especificação Tecnica</p>
+            <p>{tecs}</p>
+          </div>
+
+        </div>
+
       </section>
     );
   }
