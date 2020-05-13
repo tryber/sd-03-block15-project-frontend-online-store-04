f605b3c23d7a0bb7323e39b1bf4108f36880df3b
henrique <hbaetaleite@gmail.com>
2020-05-13 15:57:55 -0300

    Adicionando Link e Router ao componente ButtonCart

--- src/components/ButtonCart.js
+++ src/components/ButtonCart.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
 import cart from './cart.png';
 
 function ButtonCart() {
   return (
-    <button data-testid="shopping-cart-button">
-      <img src={cart} width="70px" alt="cart-icon" />
-    </button>
+    <Router>
+    <Link to="/CartPage">
+      <button data-testid="shopping-cart-button">
+        <img src={cart} width="70px" alt="cart-icon" />
+      </button>
+    </Link>
+    </Router>
   );
 }
 
