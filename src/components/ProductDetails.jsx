c2757ff2f2f92257fb45983fabad2a62f4f3f4a8
henrique <hbaetaleite@gmail.com>
2020-05-15 15:31:34 -0300

    Modificacoes

--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import ButtonCart from './ButtonCart';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
 import './ProductDetails.css';
 
-export default class ProductDetails extends Component() {
+export default class ProductDetails extends Component {
   render() {
-    const { title, price, tecs, thumbnail } = this.props;
+    const { title, price, id, thumbnail } = this.props;
     return (
       <section>
 
@@ -13,19 +14,25 @@ export default class ProductDetails extends Component() {
           <ButtonCart />
         </div>
 
-        <div className="productContainer">
-
-          <div className="imgContainer">
-            <h3 data-testid="product-detail-name">Nome do produto - {price}</h3>
-            <img src={thumbnail} alt={title} />
-          </div>
-
-          <div className="specs">
-            <p>Especificação Tecnica</p>
-            <p>{tecs}</p>
-          </div>
-
-        </div>
+        <div data-testid="product">
+          <h3>{title}</h3>
+          <h4>{id}</h4>
+          <img src={thumbnail} alt={title} />
+          <h4>
+            {`Preço: R$ ${price.toFixed(2)}`}
+          </h4>
+          {/* <h4>
+            {`Quantidade disponível: ${availableQuantity}`}
+          </h4>
+          {freeShipping && <h6 data-testid="free-shipping">FRETE GRÁTIS</h6>}
+          {this.addToCartButton()} */}
+          <Link
+            data-testid="product-detail-link"
+            to={{ pathname: `/details/${id}` }}
+          >
+            Detalhes
+          </Link>
+      </div>
 
       </section>
     );
